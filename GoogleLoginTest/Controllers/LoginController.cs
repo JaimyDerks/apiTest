dd15954b2cb2d10398cce13f70c67b7a28e18393
JaimyDerks <78917274+JaimyDerks@users.noreply.github.com>
2023-03-28 12:22:28 +0200

    added files

---
+++ GoogleLoginTest/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using GoogleLoginTest.Models;
+
+namespace GoogleLoginTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpGet]
+        public string Test()
+        {
+            TestObject testObject = new TestObject("test", 1);
+            string json = JsonConvert.SerializeObject(testObject);
+            return json;
+        }
+    }
+}
---
+++ GoogleLoginTest/Models/TestObject.cs
@@ -0,0 +1,14 @@
+﻿namespace GoogleLoginTest.Models
+{
+    public class TestObject
+    {
+        public int ID { get; set; }
+        public string value { get; set; }
+
+        public TestObject(string val, int id)
+        {
+            value = val;
+            ID = id;    
+        }
+    }
+}
